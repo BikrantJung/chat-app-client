60b22ad871215da49f55663400844e930db08581
Bikrant Jung <bikrantjung1214@gmail.com>
2023-04-19 21:30:44 +0545

    Shadcn Setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// To resolve path, pnpm add @types/node -D
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
